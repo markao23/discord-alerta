ee074dab63fc3ed34f5bd7fc0d943ccb88256cdc
markus <markuszimmermann3897@gmail.com>
2026-04-22 10:47:53 -0300

    eu to subindo tudo pois é pra deichar o webhook 24 horas de graça

---
+++ hook.js
@@ -0,0 +1,52 @@
+// Substitua pela URL que você copiou
+const express = require("express");
+const app = express();
+const webhookUrl =
+  "https://discord.com/api/webhooks/1496487271363252314/uHcpWMNCC50aIAdSyrYN_iDpUSia8p2flfbIBSvEbs44wDdiVyw0E-LHsIR25KVuNAE6";
+
+async function enviarStatusBot(status) {
+  const isOnline = status === "ON";
+  const corHex = isOnline ? 0x00ff00 : 0xff0000;
+  const mensagem = isOnline
+    ? "Preparar os canhões! O bot está online e rodando 24/7! 🚢💣"
+    : "Recolher as velas! O bot foi desligado. ⚓💤";
+
+  const data = {
+    content: "@everyone",
+    allowed_mentions: { parse: ["everyone"] },
+    embeds: [
+      {
+        title: `Status do Bot: ${status}`,
+        description: mensagem,
+        color: corHex,
+        timestamp: new Date().toISOString(),
+        footer: { text: "Captain Hook System" },
+      },
+    ],
+  };
+
+  try {
+    const response = await fetch(webhookUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (response.ok) console.log("Mensagem enviada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao enviar mensagem:", error);
+  }
+}
+
+// ROTA DA WEB: É aqui que o UptimeRobot vai acessar para manter acordado!
+app.get("/", (req, res) => {
+  res.send("O navio está navegando! 🏴‍☠️ (Servidor Online)");
+});
+
+// Ligar o Servidor Web e avisar o Discord
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+
+  // Assim que o servidor liga, ele avisa no Discord que está ON
+  enviarStatusBot("ON");
+});
